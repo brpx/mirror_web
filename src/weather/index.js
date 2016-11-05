7c6aa3f5998a5114ba017e035a5b061c7e29d463
Nuno Jesus <nunojesus@brpx.com>
2016-11-05 16:20:32 +0000

    init repo

---
+++ src/weather/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import $ from 'jquery';
+import './index.css';
+
+const API_KEY = '4f308072bff00bbec5f7db36172df29f';
+const API = ['http://api.openweathermap.org/data/2.5/weather?appid=', API_KEY, '&units=metric'].join('');
+
+function getUri(city) {
+  return [API, '&q=', city].join('');
+}
+
+class Weather extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      city: '',
+      min: 0,
+      max: 0,
+      current: 0,
+    };
+  }
+
+  componentWillMount() {
+    $.ajax(getUri('Lisbon'), {
+      method: 'GET',
+    }).done((res) => {
+      this.setState({
+        city: res.name,
+        min: Math.round(res.main.temp_min),
+        max: Math.round(res.main.temp_max),
+        current: Math.round(res.main.temp),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.city}</h1>
+        <p>Now: {this.state.current}º C</p>
+        <p>Min: {this.state.min}º C</p>
+        <p>Max: {this.state.max}º C</p>
+      </div>
+    );
+  }
+}
+
+export default Weather;
